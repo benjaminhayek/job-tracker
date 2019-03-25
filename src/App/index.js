723e92c597b48d2397f150be7d897f4ab2a142f3
bhayek12@gmail.com <bhayek12@gmail.com>
2019-03-25 12:03:39 -0600

    Import top company's to app

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Applied from '../Applied';
 import Response from '../Response';
 import Outreach from '../Outreach';
+import TopCompanys from '../TopCompanys';
 import './App.css';
 
 class App extends Component {

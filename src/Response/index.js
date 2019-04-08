5f28233be548516587881ddb4e6d941f206e70cd
bhayek12@gmail.com <bhayek12@gmail.com>
2019-04-08 14:40:28 -0600

    Add home advidor

--- src/Response/index.js
+++ src/Response/index.js
@@ -7,7 +7,7 @@ class Response extends Component {
         super(props)
         this.state = {
             name: '',
-            responses: ['The Motley Fool', 'pax8', 'oracle']
+            responses: ['The Motley Fool', 'Pax8', 'Oracle', 'Home Advisor']
         }
     }
 
